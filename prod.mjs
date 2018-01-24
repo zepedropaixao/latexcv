aeb2ba8059c9ae29c7077a34c50e17d1c20d574b
Pedro Paixão <zepedropaixao@gmail.com>
2018-01-24 17:03:16 +0000

    Changed from express to koa

--- prod.mjs
+++ prod.mjs
@@ -1,17 +1,27 @@
-import express from 'express';  
-import bodyParser from 'body-parser';
+import Koa from 'koa';  
+import serve from 'koa-static';
+import bodyParser from 'koa-bodyparser'
 import path from 'path';  
+import middl from './app/server/src/middleware'
 import expose from './expose.js';
 const {__dirname} = expose;
 
+import router from './app/server/src/routes'; 
+
 /*eslint-disable no-console */
 
-const app = express();
+const app = new Koa();
 
 console.log(__dirname);
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'app/client/dist')));
+
+app.use(middl.errorHandler());
+app.use(serve(path.join(__dirname, 'app/client/dist')));
+app.use(bodyParser());
+app.use(router);
+
+
+
 
 var port = process.env.PORT || 5000;
 
